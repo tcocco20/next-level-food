78f04c2807c5a6cfb5d7bde1b79731523db14778
tcocco20 <theo@coccoscoding.dev>
2024-07-05 16:35:09 -0700

    finished meal uploading functions

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,18 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
+export interface FormMeal {
+  title: string;
+  summary: string;
+  instructions: string;
+  image: File | string;
+  creator: string;
+  creator_email: string;
+  slug?: string;
+}
+
 export async function shareMeal(formData: FormData) {
   "use server";
 
@@ -7,10 +20,12 @@ export async function shareMeal(formData: FormData) {
     title: formData.get("title") as string,
     summary: formData.get("summary") as string,
     instructions: formData.get("instructions") as string,
-    image: formData.get("image") as string,
+    image: formData.get("image") as File,
     creator: formData.get("name") as string,
     creator_email: formData.get("email") as string,
   };
 
-  console.log(meal);
+  await saveMeal(meal);
+
+  redirect("/meals");
 }
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,6 +1,8 @@
+import fs from "node:fs";
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
+import { FormMeal } from "./actions";
 
 const db = sql("meals.db");
 
@@ -24,7 +26,29 @@ export function getMeal(slug: string): Meal {
   return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as Meal;
 }
 
-export function saveMeal(meal: Meal) {
+export async function saveMeal(meal: FormMeal) {
   meal.slug = slugify(meal.title, { lower: true });
   meal.instructions = xss(meal.instructions);
+
+  if (!(meal.image instanceof File)) return;
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Failed to save image.");
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  db.prepare(
+    `
+    INSERT INTO meals (title, slug, image, summary, creator, creator_email, instructions)
+    VALUES (@title, @slug, @image, @summary, @creator, @creator_email, @instructions)`
+  ).run(meal);
 }
